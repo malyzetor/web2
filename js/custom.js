1d7ebddaf8f7b9db9b4ed7a485b24189f2eecf1d
Pavel Jäger <jager@desktop.lan>
2017-11-20 20:40:07 +0100

    další redukce kódu

--- js/custom.js
+++ js/custom.js
@@ -20,26 +20,7 @@ $(document).ready(function () {
         }
     });
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 400) {
-            $("#navigation").css("background-color", "rgba(16, 22, 16,1)");
-        } else {
-            $("#navigation").css("background-color", "rgba(16, 22, 54, 0.2)");
-        }
-    });
 
-    /* ========================================================================= */
-	/*	Fix Slider Height
-	/* ========================================================================= */
-
-    var slideHeight = $(window).height();
-
-    $('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height', slideHeight);
-
-    $(window).resize(function () {
-        'use strict',
-        $('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height', slideHeight);
-    });
 
 
    
