afed0ad8f9500fc217527ca1fe8118847980efde
Pavel Jäger <jager@linux-p3lv.suse>
2020-08-21 09:33:07 +0200

    1968

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
 	</header>
 
 <!-- dynamicky generované heslo dne .-->
-	<div id="jHD"><?php include('./HD-2007-jiri-dekovna.inc'); ?></div>
+	<div id="jHD"><?php include('./HD-2008-1968.inc'); ?></div>
 
 	<section id="Farnosti" class="features"  style="display:flex; min-height: 100vh;">
 		<div class="container" style="margin-top: auto; margin-bottom: auto;">
@@ -170,18 +170,8 @@
 				<div class="sec-title text-center mb50">
 					<h2>Pořad bohoslužeb</h2>
 				</div>
-				<div class="col-md-6">
-					<div class="service-item">
-						<div class="service-icon" style="width: 100px;">
-							<a href="./pdf/2020_08_09-plakat-mse_s_rodinami.pdf" style="color:#8C2318;" title="Pěší pouť do Loukova"><img src="img/bota.svg" style="width: 5em;" class="rounded float-right" alt="Pouť" /></a>
-						</div>
-						<div class="service-desc">
-							<h3>Pěší pouť do Loukova</h3>
-							<p>Neděle 9. srpna 2020&nbsp;&nbsp;<a href="./pdf/2020_08_09-plakat-mse_s_rodinami.pdf" style="color:#8C2318;" title="Pěší pouť do Loukova"><i class="far fa-file-pdf"></i></a>
-							</p>
-						</div>
-					</div>
-				</div>
+				<div class="col-md-3"></div>
+				
 				<div class="col-md-6">
 					<div class="service-item">
 						<div class="service-icon" style="width: 100px;">
