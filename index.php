94eb93cae6ee7059f0c57874861c3aabcd15e518
malyzetor <pavel.jager@gmail.com>
2022-02-21 10:34:48 +0100

    nešpory

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 			<nav class="collapse navbar-collapse navbar-right">
 				<ul id="nav" class="nav navbar-nav">
 				<li><a href="#Synoda">Synoda21-23</a></li>
-				<li><a href="vespers/2022_02_11.html"><img src="img/apokalypsa.svg" alt="Hodinová apokalypsa" style="width: 1.5em;"></a></li>
+				<li><a href="vespers/2022_02_25.html"><img src="img/apokalypsa.svg" alt="Hodinová apokalypsa" style="width: 1.5em;"></a></li>
 				<li>
 						<a href="#Bohosluzby">Pořad bohoslužeb</a>
 					</li>
